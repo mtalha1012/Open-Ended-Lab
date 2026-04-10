3a1ad7caace8beb6a522250a0b12e0e79e0a11fd
= <=>
2026-04-10 19:32:47 +0500

    Recipe class initial structure done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,65 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    }
+}
+
+interface Ratable {
+
+}
+
+class Recipe implements Ratable {
+    // Attributes
+    private String name;
+    private String description;
+    private String instructions;
+
+    // Constructors
+    public Recipe() {
+    }
+
+    public Recipe(String name, String description, String instructions) {
+        this.name = name;
+        this.description = description;
+        this.instructions = instructions;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    // Overriding equals method
+    @Override
+    public boolean equals(Object obj) {
+        Recipe recipe;
+        if (obj instanceof Recipe)
+            recipe = (Recipe) obj;
+        else
+            return false;
+        return (this.name.equalsIgnoreCase(recipe.getName()) &&
+                this.description.equalsIgnoreCase(recipe.getDescription()) &&
+                this.instructions.equalsIgnoreCase(recipe.getInstructions()));
     }
 }
